26288747d3e55cec08c1aa64b471104982d0b36f
WengSJ <497012694@qq.com>
2019-02-18 22:00:27 +0800

    服务注册

---
+++ eureka-client/src/main/java/com/wengsj/eurekaclient/ctrl/HiController.java
@@ -0,0 +1,17 @@
+package com.wengsj.eurekaclient.ctrl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public  String home(@RequestParam(value = "name", defaultValue = "forezp") String name){
+        return  "hi "+name+",i an from port"+port;
+
+    }
+}
