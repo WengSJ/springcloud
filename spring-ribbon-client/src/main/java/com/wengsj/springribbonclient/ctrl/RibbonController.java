09cc15ce80837731592939100bc7fc53a4873485
WengSJ <497012694@qq.com>
2019-02-21 18:09:37 +0800

    负载均衡

---
+++ spring-ribbon-client/src/main/java/com/wengsj/springribbonclient/ctrl/RibbonController.java
@@ -0,0 +1,27 @@
+package com.wengsj.springribbonclient.ctrl;
+
+import com.wengsj.springribbonclient.service.RibbonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RibbonController {
+    @Autowired
+    RibbonService ribbonService;
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+    @GetMapping("/hi")
+    public  String hi(@RequestParam(required =false,defaultValue = "wengsj")String name){
+        return  ribbonService.hi(name);
+    }
+
+    @GetMapping("/testRibbon")
+    public  String testRibbon(){
+        ServiceInstance instance=loadBalancerClient.choose("eureka-client");
+        return  instance.getHost()+":"+instance.getPort();
+    }
+}
