abac55fe16740b1f705a53a20a16d22ac4965f61
WengSJ <497012694@qq.com>
2019-02-22 10:01:41 +0800

    负载均衡

---
+++ spring-ribbon-client/src/main/java/com/wengsj/springribbonclient/SpringRibbonClientApplication.java
@@ -0,0 +1,15 @@
+package com.wengsj.springribbonclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringRibbonClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRibbonClientApplication.class, args);
+    }
+
+}
